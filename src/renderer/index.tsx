a25e79ec5a0c3a2568aea36cd6af775ec75d6f57
danikaze <danikaze@gmail.com>
2024-08-18 22:58:36 +0900

    Fix TS paths and code standards

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,8 +1,9 @@
-import './assets/main.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+
+import { App } from './app';
+
+import './assets/main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
