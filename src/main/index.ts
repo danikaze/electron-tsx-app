d130f2935e4af3c165f676282ab9dd59acc96554
danikaze <danikaze@gmail.com>
2024-08-18 21:29:58 +0900

    Apply new eslint rules

--- src/main/index.ts
+++ src/main/index.ts
@@ -45,7 +45,7 @@ app.whenReady().then(() => {
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
+  app.on('browser-window-created', (ev, window) => {
     optimizer.watchWindowShortcuts(window);
   });
 
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import Versions from './components/Versions';
+import Versions from './components/versions';
 import electronLogo from './assets/electron.svg';
 
 function App(): JSX.Element {
